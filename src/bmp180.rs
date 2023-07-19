7966a29e0d0fd4efaaec716ee1240aea06f21be1
bjoernQ <bjoern.quentin@mobile-j.de>
2023-07-19 15:45:37 +0200

    Improvements

--- src/bmp180.rs
+++ src/bmp180.rs
@@ -1,133 +1,14 @@
-use embedded_hal::blocking::i2c::*;
+use core::future::Future;
 
 const ADDRESS: u8 = 0x77;
 
-// pub struct Bmp180<T>
-// where
-//     T: WriteRead + Read + Write,
-// {
-//     i2c: T,
-//     ac1: i32,
-//     ac2: i32,
-//     ac3: i32,
-//     ac4: i32,
-//     ac5: i32,
-//     ac6: i32,
-//     b1: i32,
-//     b2: i32,
-//     mb: i32,
-//     mc: i32,
-//     md: i32,
-
-//     temp: f32,
-
-//     // sleep_fn: fn(u32) -> (),
-// }
-
-// impl<T> Bmp180<T>
-// where
-//     T: WriteRead + Read + Write,
-// {
-//     pub fn new(i2c: T/*, sleep_fn: fn(u32) -> ()*/) -> Bmp180<T> {
-//         let mut i2c = i2c;
-//         let mut data = [0u8; 22];
-
-//         i2c.write_read(ADDRESS, &[0xaa], &mut data).ok();
-
-//         let ac1 = ((data[0] as u16) << 8 | data[1] as u16) as i16 as i32;
-//         let ac2 = ((data[2] as u16) << 8 | data[3] as u16) as i16 as i32;
-//         let ac3 = ((data[4] as u16) << 8 | data[5] as u16) as i16 as i32;
-//         let ac4 = ((data[6] as u16) << 8 | data[7] as u16) as i16 as i32;
-//         let ac5 = ((data[8] as u16) << 8 | data[8] as u16) as i16 as i32;
-//         let ac6 = ((data[10] as u16) << 8 | data[11] as u16) as i16 as i32;
-//         let b1 = ((data[12] as u16) << 8 | data[13] as u16) as i16 as i32;
-//         let b2 = ((data[14] as u16) << 8 | data[15] as u16) as i16 as i32;
-//         let mb = ((data[16] as u16) << 8 | data[17] as u16) as i16 as i32;
-//         let mc = ((data[18] as u16) << 8 | data[19] as u16) as i16 as i32;
-//         let md = ((data[20] as u16) << 8 | data[21] as u16) as i16 as i32;
-
-//         Self {
-//             i2c,
-//             ac1,
-//             ac2,
-//             ac3,
-//             ac4,
-//             ac5,
-//             ac6,
-//             b1,
-//             b2,
-//             mb,
-//             mc,
-//             md,
-
-//             temp: 0f32,
-
-//             // sleep_fn,
-//         }
-//     }
-
-//     pub fn measure(&mut self) {
-//         // Select measurement control register
-//         // Enable temperature measurement
-//         self.i2c.write(ADDRESS, &[0xf4, 0x2e]).ok();
-//         // (self.sleep_fn)(100);
-
-//         // Read 2 bytes of data from address 0xF6(246)
-//         // temp msb, temp lsb
-//         let mut data = [0u8; 2];
-//         self.i2c.write_read(ADDRESS, &[0xF6], &mut data).ok();
-
-//         // Convert the data
-//         let temp = (data[0] as u32) << 8 | data[1] as u32;
-
-//         // Select measurement control register
-//         // Enable pressure measurement, OSS = 1
-//         self.i2c.write(ADDRESS, &[0xf4, 0x74]).ok();
-//         // (self.sleep_fn)(100);
-
-//         // Callibration for Temperature
-//         let x1: f64 = (temp as f64 - self.ac6 as f64) * self.ac5 as f64 / 32768.0;
-//         let x2: f64 = (self.mc as f64 * 2048.0) / (x1 + self.md as f64);
-//         let b5: f64 = x1 + x2;
-//         let c_temp: f64 = ((b5 + 8.0) / 16.0) / 10.0;
-
-//         self.temp = c_temp as f32;
-//     }
-
-//     pub fn get_temperature(&self) -> f32 {
-//         self.temp
-//     }
-// }
-
-impl<T> core::fmt::Debug for Bmp180<T>
-where
-    T: WriteRead + Read + Write + hal::prelude::_embedded_hal_async_i2c_I2c,
-{
-    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-        f.debug_struct("Bmp180")
-            .field("ac1", &self.ac1)
-            .field("ac2", &self.ac2)
-            .field("ac3", &self.ac3)
-            .field("ac4", &self.ac4)
-            .field("ac5", &self.ac5)
-            .field("ac6", &self.ac6)
-            .field("b1", &self.b1)
-            .field("b2", &self.b2)
-            .field("mb", &self.mb)
-            .field("mc", &self.mc)
-            .field("md", &self.md)
-            .field("temp", &self.temp)
-            .finish()
-    }
-}
-
-// #[cfg(feature = "async")]
-// pub mod asynch {
 use embedded_hal_async::i2c::I2c;
 use esp_println::println;
-pub struct Bmp180<T>
+pub struct Bmp180<T, F, R>
 where
     T: I2c,
+    F: Fn(u32) -> R,
+    R: Future<Output = ()>,
 {
     i2c: T,
     ac1: i32,
@@ -143,22 +24,21 @@ where
     md: i32,
 
     temp: f32,
-    // sleep_fn: fn(u32) -> (),
+    sleep_fn: F,
 }
 
-impl<T> Bmp180<T>
+impl<T, F, R> Bmp180<T, F, R>
 where
     T: I2c,
+    F: Fn(u32) -> R,
+    R: Future<Output = ()>,
 {
-    pub async fn new(i2c: T /*, sleep_fn: fn(u32) -> ()*/) -> Bmp180<T> {
-        println!("async"); // printed
+    pub async fn new(i2c: T, sleep_fn: F) -> Bmp180<T, F, R> where {
         let mut i2c = i2c;
         let mut data = [0u8; 22];
 
-        println!("async"); // printed
-        i2c.write_read(ADDRESS, &[0xaa], &mut data).await.ok();
+        i2c.write_read(ADDRESS, &[0xaa], &mut data).await.unwrap();
 
-        println!("async"); // not printed
         let ac1 = ((data[0] as u16) << 8 | data[1] as u16) as i16 as i32;
         let ac2 = ((data[2] as u16) << 8 | data[3] as u16) as i16 as i32;
         let ac3 = ((data[4] as u16) << 8 | data[5] as u16) as i16 as i32;
@@ -186,27 +66,26 @@ where
             md,
 
             temp: 0f32,
-            // sleep_fn,
+            sleep_fn,
         }
     }
 
     pub async fn measure(&mut self) {
         // Select measurement control register
         // Enable temperature measurement
-        self.i2c.write(ADDRESS, &[0xf4, 0x2e]).await.ok();
-        println!("measure??");
+        self.i2c.write(ADDRESS, &[0xf4, 0x2e]).await.unwrap();
+        (self.sleep_fn)(100).await;
 
         // Read 2 bytes of data from address 0xF6(246)
         // temp msb, temp lsb
         let mut data = [0u8; 2];
-        self.i2c.write_read(ADDRESS, &[0xF6], &mut data).await.ok();
+        self.i2c
+            .write_read(ADDRESS, &[0xF6], &mut data)
+            .await
+            .unwrap();
 
         // Convert the data
         let temp = (data[0] as u32) << 8 | data[1] as u32;
-
-        // Select measurement control register
-        // Enable pressure measurement, OSS = 1
-        self.i2c.write(ADDRESS, &[0xf4, 0x74]).await.ok();
 
         // Calibration for Temperature
         let x1: f64 = (temp as f64 - self.ac6 as f64) * self.ac5 as f64 / 32768.0;
@@ -216,5 +95,8 @@ where
 
         self.temp = c_temp as f32;
     }
+
+    pub fn get_temperature(&self) -> f32 {
+        self.temp
+    }
 }
-// }
--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,14 @@ use embedded_hal::watchdog::WatchdogDisable;
 use esp_backtrace as _;
 use esp_println::println;
 use hal::clock::ClockControl;
-// use hal::clock::CpuClock;
-use hal::embassy;
 use hal::i2c::I2C;
-use hal::peripherals::{Peripherals, I2C0};
+use hal::peripherals::{Interrupt, Peripherals, I2C0};
 use hal::prelude::_fugit_RateExtU32;
 use hal::prelude::*;
 use hal::timer::TimerGroup;
 use hal::Rtc;
 use hal::IO;
+use hal::{embassy, interrupt};
 
 use embassy_executor::Executor;
 use embassy_time::{Duration, Timer};
@@ -23,23 +22,18 @@ use embassy_time::{Duration, Timer};
 use static_cell::StaticCell;
 
 use crate::bmp180::Bmp180;
-// #[cfg(feature = "async")]
-// use crate::bmp180::asynch;
 mod bmp180;
 
 const INTERVALL_MS: u64 = 1 * 60 * 1000; // 1 minute intervall
 
 #[embassy_executor::task]
 async fn measure(i2c: I2C<'static, I2C0>) {
-    println!("Hello?"); //printed
-    let mut bmp = Bmp180::new(i2c).await;
-    println!("Hello?"); //printed
+    let mut bmp = Bmp180::new(i2c, sleep).await;
 
     loop {
-        bmp.measure().await; //looks wrong, move error
-
+        bmp.measure().await;
+        println!("{}", bmp.get_temperature());
         Timer::after(Duration::from_millis(1000)).await;
-        println!("Hello?"); //printed
     }
 }
 
@@ -47,7 +41,7 @@ static EXECUTOR: StaticCell<Executor> = StaticCell::new();
 
 #[entry]
 fn main() -> ! {
-    init_logger();
+    esp_println::logger::init_logger_from_env();
 
     let peripherals = Peripherals::take();
     let mut system = peripherals.SYSTEM.split();
@@ -74,54 +68,31 @@ fn main() -> ! {
     wdt0.disable();
     wdt1.disable();
 
-    println!("preinit");
-
-    #[cfg(feature = "embassy-time-systick")]
     embassy::init(
         &clocks,
         hal::systimer::SystemTimer::new(peripherals.SYSTIMER),
     );
-    println!("postinit");
     let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
 
     // Create a new peripheral object with the described wiring
     // and standard I2C clock speed
     let i2c0 = I2C::new(
         peripherals.I2C0,
-        io.pins.gpio10,
-        io.pins.gpio8,
+        io.pins.gpio1,
+        io.pins.gpio2,
         100u32.kHz(),
         &mut system.peripheral_clock_control,
         &clocks,
     );
 
+    interrupt::enable(Interrupt::I2C_EXT0, interrupt::Priority::Priority1).unwrap();
+
     let executor = EXECUTOR.init(Executor::new());
     executor.run(|spawner| {
         spawner.spawn(measure(i2c0)).ok();
     });
-
-    // loop {println!("Measuring");}
-    
 }
 
-pub fn init_logger() {
-    unsafe {
-        log::set_logger_racy(&LOGGER).unwrap();
-        log::set_max_level(log::LevelFilter::Info);
-    }
-}
-
-static LOGGER: SimpleLogger = SimpleLogger;
-struct SimpleLogger;
-
-impl log::Log for SimpleLogger {
-    fn enabled(&self, _metadata: &log::Metadata) -> bool {
-        true
-    }
-
-    fn log(&self, record: &log::Record) {
-        println!("{} - {}", record.level(), record.args());
-    }
-
-    fn flush(&self) {}
+pub async fn sleep(millis: u32) {
+    Timer::after(Duration::from_millis(millis as u64)).await;
 }
